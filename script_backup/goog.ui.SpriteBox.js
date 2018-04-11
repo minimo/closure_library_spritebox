093912472a5a8fa4fd86503a1efab2f5bf449956
minimo <minimo.mobile@gmail.com>
2018-04-11 12:26:04 +0900

    Update.

--- script_backup/goog.ui.SpriteBox.js
+++ script_backup/goog.ui.SpriteBox.js
@@ -4,17 +4,13 @@ goog.provide('goog.ui.SpriteBox');
 //依存クラス宣言
 goog.require('goog.ui.Component');
 goog.require('goog.events.EventHandler');
-goog.require('goog.ui.SpriteBoxItem');
 
 //コンストラクタ
-goog.ui.SpriteBox = function(param, opt_label, opt_domHelper){
+goog.ui.SpriteBox = function(opt_label, opt_domHelper){
   goog.ui.Component.call(this,opt_domHelper); //基底クラスのコンストラクタ呼び出し
 
   this.label_ = opt_label || 'Click Me';
   this.eh_ = new goog.events.EventHandler(this);
-
-  //パラメータ取得
-  this.param_ = param;
 
   this.initialize_();
 }
@@ -31,16 +27,37 @@ goog.ui.SpriteBox.prototype.createDom = function(){
   //大元のDivを作成
   var rootDiv = this.dom_.createElement('div');
   this.decorateInternal(rootDiv);
-
-  var item = new goog.ui.SpriteBoxItem();
-  this.addItem(item);
-  var item = new goog.ui.SpriteBoxItem();
-  this.addItem(item);
 };
 
 goog.ui.SpriteBox.prototype.decorateInternal = function(element){
   //closureのuiコンポーネントはDOMのルートを保存しておく必要がある 
   this.setElementInternal(element);
+
+  //スタイルの設定
+  this.dom_.setProperties(element, {
+    "style": "border: 1px solid black; width: 150px; background-color: gray; color: white; text-align: center; font-weight: bold;"
+  });
+
+  var img = this.dom_.createElement('img');
+  img.setAttribute("src", "assets/item_apple.png");
+  img.setAttribute("width", "32");
+  img.setAttribute("height", "32");
+  element.appendChild(img);
+
+  if(!this.getLabelText()){
+    this.setLabelText(this.label_);
+  }
+};
+
+goog.ui.SpriteBox.prototype.setLabelText = function(text){
+  if(this.getElement()){
+    this.dom_.setTextContent(this.getElement(), text);
+  }
+};
+
+goog.ui.SpriteBox.prototype.getLabelText = function(){
+  if(!this.getElement()) return '';
+  return this.dom_.getTextContent(this.getElement());
 };
 
 goog.ui.SpriteBox.prototype.enterDocument = function(){
@@ -56,6 +73,10 @@ goog.ui.SpriteBox.prototype.exitDocument = function(){
 goog.ui.SpriteBox.prototype.disposeInternal = function(){
   goog.base(this, "disposeInternal");
   this.eh_.dispose();
+};
+
+goog.ui.SpriteBox.prototype.onDivClicked_ = function(event){
+  alert(this.label_);
 };
 
 goog.ui.SpriteBox.prototype.addItem = function(item) {
---
+++ scripts/goog.ui.SpriteBoxItem.js
@@ -0,0 +1,65 @@
+
+//自クラスの名称宣言
+goog.provide('goog.ui.SpriteBoxItem');
+
+//依存クラス宣言
+goog.require('goog.ui.Component');
+goog.require('goog.events.EventHandler');
+
+//コンストラクタ
+goog.ui.SpriteBoxItem = function(src, opt_label, opt_domHelper){
+  goog.ui.Component.call(this, opt_domHelper); //基底クラスのコンストラクタ呼び出し
+  this.eh_ = new goog.events.EventHandler(this);
+  this.initialize_();
+}
+
+//goog.ui.Componentから継承
+goog.inherits(goog.ui.SpriteBoxItem, goog.ui.Component);
+
+//クラス初期化
+goog.ui.SpriteBoxItem.prototype.initialize_ = function(){
+};
+
+//DOMの構築（必須）
+goog.ui.SpriteBoxItem.prototype.createDom = function(){
+  //大元のDivを作成
+  var rootDiv = this.dom_.createElement('div');
+  this.decorateInternal(rootDiv);
+};
+
+goog.ui.SpriteBoxItem.prototype.decorateInternal = function(element){
+  //closureのuiコンポーネントはDOMのルートを保存しておく必要がある 
+  this.setElementInternal(element);
+
+  //スタイルの設定
+  this.dom_.setProperties(element, {
+//    "style": "border: 1px solid black; width: 32px; background-color: gray; color: white; text-align: center; font-weight: bold; display: inline-block"
+    "style": "display: inline-block"
+  });
+
+  var img = this.dom_.createElement('img');
+  img.setAttribute("src", "assets/item_apple.png");
+  img.setAttribute("width", "32");
+  img.setAttribute("height", "32");
+  element.appendChild(img);
+};
+
+goog.ui.SpriteBoxItem.prototype.enterDocument = function(){
+  goog.base(this, "enterDocument");
+  this.eh_.listen(this.getElement(), goog.events.EventType.CLICK, this.onDivClicked_);
+};
+
+goog.ui.SpriteBoxItem.prototype.exitDocument = function(){
+  goog.base(this, "exitDocument");
+  this.eh_.unlisten(this.getElement(), goog.events.EventType.CLICK, this.onDivClicked_);
+};
+
+goog.ui.SpriteBoxItem.prototype.disposeInternal = function(){
+  goog.base(this, "disposeInternal");
+  this.eh_.dispose();
+};
+
+goog.ui.SpriteBoxItem.prototype.onDivClicked_ = function(event){
+  alert("ClickItem");
+};
+
